f9969e1c8ce6654d2e8de23319c28f45a41991a9
JayeshVP24 <jayeshpotlabattini@gmail.com>
2024-02-13 21:12:51 +0530

    feat: posthog analytics setup done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+    import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
     import Footer from '$lib/components/Footer.svelte';
     import '../app.pcss';
     import { ModeWatcher } from 'mode-watcher';
+    import posthog from 'posthog-js';
+    import { page } from '$app/stores';
+    import { browser } from '$app/environment';
+
+    if (browser) {
+        posthog.init(PUBLIC_POSTHOG_API_KEY, {
+            api_host: $page.url.origin + '/wegotanalyticsbaby',
+            ui_host: 'https://us.posthog.com'
+        });
+    }
 </script>
 
 <ModeWatcher />
