19aab438b21a5658459c4cbc1ffde53a9a0d7759
JayeshVP24 <jayeshpotlabattini@gmail.com>
2024-02-14 15:28:36 +0530

    chore: favicons and OG image added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import SvelteSeo from 'svelte-seo';
     import { PUBLIC_POSTHOG_API_KEY } from '$env/static/public';
     import Footer from '$lib/components/Footer.svelte';
     import '../app.pcss';
@@ -14,6 +15,38 @@
         });
     }
 </script>
+
+<SvelteSeo
+    title="FontAlt | Alternatives for your favorite fonts"
+    description="Tired of using the same boring font? Here are alternatives for your favourite fonts – whether it's Inter or Comic Sans"
+    canonical="https://fontalt.com/"
+    keywords="fonts, alternative fonts, font alternatives, alternative, best fonts"
+    openGraph={{
+        title: 'FontAlt | Alternatives for your favorite fonts',
+        description:
+            "Tired of using the same boring font? Here are alternatives for your favourite fonts – whether it's Inter or Comic Sans",
+        image: 'https://fontalt.com/fontalt-og.png',
+        url: 'https://fontalt.com/',
+        type: 'website',
+        images: [
+            {
+                url: 'https://fontalt.com/fontalt-og.png',
+                width: 1200,
+                height: 630,
+                alt: 'Alternative to your favorite fonts'
+            }
+        ],
+        site_name: 'Font Alt'
+    }}
+    twitter={{
+        card: 'summary_large_image',
+        site: '@kuxshl',
+        title: 'FontAlt | Alternatives for your favorite fonts',
+        description:
+            "Tired of using the same boring font? Here are alternatives for your favourite fonts – whether it's Inter or Comic Sans",
+        image: 'https://fontalt.com/fontalt-og.png'
+    }}
+/>
 
 <ModeWatcher />
 <slot />
